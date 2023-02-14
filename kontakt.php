4a3ee4633df66e914b217ac19040648d7cbb2e01
darko2019 <darko.ilic83@gmail.com>
2023-02-14 13:50:16 +0100

    modifikovana kontakt forma i mejler

--- kontakt.php
+++ kontakt.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
     $name = $_POST['name'];
-    $surname = $_POST['surname'];
+    $company = $_POST['company'];
     $email = $_POST['email'];
     $telefon = $_POST['telefon'];
     $message = $_POST['message'];
@@ -9,11 +9,11 @@ if (isset($_POST["submit"])) {
 
     $to = 'darko.ilic83@gmail.com';
     $subject = 'Contact form';
-    $body = "Ime: $name\r\n\nPrezime: $surname\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
+    $body = "Ime: $name\r\n\nKompanija: $company\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
 
 // More headers
-    $headers = 'From: ' . $name . " " . $surname . ' <' . $email . '>' . "\r\n";
-    $headers .= 'Reply-To: ' . $name . " " . $surname . ' <' . $email . '>' . "\r\n";
+    $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+    $headers .= 'Reply-To: ' . $name . ' <' . $email . '>' . "\r\n";
     if (mail($to, $subject, $body, $headers)) {
         $result = true;
     }
